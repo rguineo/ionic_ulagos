be01cfa0d17c3f3c7b651736588c6ed9b33d50d0
Roberto Guineo <rguineoa@gmail.com>
2022-11-08 23:21:59 -0400

    primer commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,41 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+destinos: any[] = [
+  {
+    "imagen": "assets/img/1.jpg",
+    "ciudad": "Natales",
+    "pais": "Chile",
+    "zona": "Patagonia",
+    "fecha": "1 - 15 dic",
+    "precio": "175.000 CLP"
+  },
+  {
+    "imagen": "assets/img/2.jpg",
+    "ciudad": "Punta Arenas",
+    "pais": "Chile",
+    "zona": "Magallanes",
+    "fecha": "10 - 15 ene",
+    "precio": "275.000 CLP"
+  },
+  {
+    "imagen": "assets/img/3.jpg",
+    "ciudad": "Coyhaique",
+    "pais": "Chile",
+    "zona": "Patagonia",
+    "fecha": "8 - 21 feb",
+    "precio": "375.000 CLP"
+  },
+  {
+    "imagen": "assets/img/4.jpg",
+    "ciudad": "Aysen",
+    "pais": "Chile",
+    "zona": "Patagonia",
+    "fecha": "20 - 25 mar",
+    "precio": "255.000 CLP"
+  },
+];
+
   constructor() {}
 
 }
