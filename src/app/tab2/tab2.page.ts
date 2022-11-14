6bd8e495def710cd60342a9cbbd957e1cb7eec9d
Roberto Guineo <rguineoa@gmail.com>
2022-11-14 10:44:23 -0400

    actualizacion

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
+  favoritos: any[] = [
+    {
+      "imagen": "assets/img/1.jpg",
+      "ciudad": "Natales"
+    },
+    {
+      "imagen": "assets/img/2.jpg",
+      "ciudad": "Punta Arenas"
+    },
+    {
+      "imagen": "assets/img/3.jpg",
+      "ciudad": "Coyhaique"
+    },
+    {
+      "imagen": "assets/img/4.jpg",
+      "ciudad": "Aysen"
+    },
+  ];
+  
   constructor() {}
 
 }
